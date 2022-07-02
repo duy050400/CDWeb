2d06359b7a46c80c3c580b21e9f0f2e890d75c7f
duy050400 <81029230+duy050400@users.noreply.github.com>
2022-07-02 23:22:17 +0700

    .

--- backend/src/main/java/com/movieticket/backend/controller/TestController.java
+++ backend/src/main/java/com/movieticket/backend/controller/TestController.java
@@ -1,15 +1,24 @@
 package com.movieticket.backend.controller;
 
+import com.movieticket.backend.entity.User;
+import com.movieticket.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Optional;
+
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+	@Autowired
+	UserRepository userRepository;
 	@GetMapping("/all")
 	public String allAccess() {
 		return "Public Content.";
@@ -22,14 +31,16 @@ public class TestController {
 	}
 
 	@GetMapping("/mod")
-	@PreAuthorize("hasRole('MODERATOR')")
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
 	public String moderatorAccess() {
 		return "Moderator Board.";
 	}
 
 	@GetMapping("/admin")
 	@PreAuthorize("hasRole('ADMIN')")
-	public String adminAccess() {
-		return "Admin Board.";
+	public List<User> listUser(){
+		List<User> user = userRepository.findAll();
+		return user;
 	}
+
 }
--- backend/src/main/java/com/movieticket/backend/entity/Province.java
+++ backend/src/main/java/com/movieticket/backend/entity/Province.java
@@ -1,6 +1,6 @@
 package com.movieticket.backend.entity;
 
-import javax.annotation.Generated;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
