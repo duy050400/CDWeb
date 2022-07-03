1b482b4e8e9472261ace2e01fb58fd87d1938bc2
duy050400 <81029230+duy050400@users.noreply.github.com>
2022-07-03 23:44:15 +0700

    SQL

--- SQL.sql
+++ SQL.sql
@@ -1,48 +1,18 @@
-CREATE DATABASE Booking_Movie_Ticket
-GO
-Use Booking_Movie_Ticket
-GO
---CREATE TABLE Date_Exam
---(
---	id int IDENTITY(1,1) PRIMARY KEY,
---	trailer nvarchar(50) NOT NULL FOREIGN KEY REFERENCES Semester (ID_Semester),
---	ID_Schedule nvarchar(50) NOT NULL FOREIGN KEY REFERENCES Schedule (ID_Schedule),
---	group_Exam nvarchar(50) ,
---	seats smallint  not null,
---)
+SELECT * FROM moviet_ticket.province;
+SELECT * FROM moviet_ticket.cinema;
 
-CREATE TABLE Movies
-(
-	id int IDENTITY(1,1) PRIMARY KEY,
-	trailer nvarchar(50) NOT NULL,
-	iamge image NOT NULL,
-	title nvarchar(256) NOT NULL ,
-	descriptions nvarchar(MAX) NOT NULL ,
-	rated nvarchar(256) NOT NULL ,
-	in_theater nvarchar(256) NOT NULL ,
-	genre nvarchar(256) NOT NULL ,
-	director nvarchar(256) NOT NULL ,
-	casts nvarchar(256) NOT NULL ,
-	runtime time(7) NOT NULL ,
-	languages nvarchar(256) NOT NULL ,
-)
+INSERT INTO moviet_ticket.roles(name) VALUES('ROLE_USER');
+INSERT INTO moviet_ticket.roles(name) VALUES('ROLE_MODERATOR');
+INSERT INTO moviet_ticket.roles(name) VALUES('ROLE_ADMIN');
 
-CREATE TABLE Users
-(
-	phone_number varchar(11) PRIMARY KEY,
-	[password] nvarchar(256) NOT NULL,
-	[name] nvarchar(256) NOT NULL,
-	email nvarchar(256),
-	date_of_birth date NOT NULL,
-	sex nvarchar(50) NOT NULL
-)
+insert into moviet_ticket.province(name) values('Đồng Nai');
+insert into moviet_ticket.province(name) values('Hồ Chí Minh');
+insert into moviet_ticket.province(name) values('Bình Dương');
 
-CREATE TABLE Auditorium
-(
-	phone_number varchar(11) PRIMARY KEY,
-	[password] nvarchar(256) NOT NULL,
-	[name] nvarchar(256) NOT NULL,
-	email nvarchar(256),
-	date_of_birth date NOT NULL,
-	sex nvarchar(50) NOT NULL
-)
+
+INSERT INTO `moviet_ticket`.`cinema` (`address`, `name`, `province_id`) VALUES ('320 Đồng Khởi', 'Đồng Khởi Cinema', '1');
+INSERT INTO `moviet_ticket`.`cinema` (`address`, `name`, `province_id`) VALUES ('1024 Đồng Khởi', 'Đồng Khởi Cinema', '1');
+INSERT INTO `moviet_ticket`.`cinema` (`address`, `name`, `province_id`) VALUES ('280 Võ Thị Sáu', 'Võ Thị Sáu Cinema', '2');
+
+select c.name , p.name from moviet_ticket.province p join moviet_ticket.cinema c where c.province_id=p.id;
+select c.id,c.name , c.address,p.name from moviet_ticket.province p join moviet_ticket.cinema c where c.province_id=p.id;
